c1a6b7d864abf6230092604c4b5eddaee1df72ca
JzjSunshine <1694162997@qq.com>
2023-08-12 18:30:01 +0800

    添加测试脚本

---
+++ trainer/test.sh
@@ -0,0 +1,3 @@
+python test.py --checkpoint ./saved/models/PICK_Default/best/model_best.pth --boxes_transcripts ./data/test_data_example/boxes_and_transcripts/ \
+               --images_path ./data/test_data_example/images --output_folder ./test_output \
+               --gpu 0 --batch_size 2
